d041876847e8f47a050de69d8b8c353de9bed2b6
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-07-03 18:35:24 -0400

    Adds MovieDatail

---
+++ src/components/MovieDetail.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const MovieDetail = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/movies/${id}`)
+      .then(response => response.json())
+      .then(data => setMovie(data));
+  }, [id]);
+
+  if (!movie) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      <p>Genre: {movie.genre}</p>
+      <p>Series: {movie.series}</p>
+      <p>Rating: {movie.rating}</p>
+    </div>
+  );
+}
+
+export default MovieDetail;
