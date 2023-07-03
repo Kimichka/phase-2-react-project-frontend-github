b506692d70eae024d4fc79ea58084f910ea8e092
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-07-03 18:50:44 -0400

    Updates App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import MovieDetail from './components/MovieDetail';
@@ -9,13 +9,11 @@ function App() {
     <Router>
       <div>
         <Header />
-        <Switch>
-          <Route path="/" exact>
-            <h1>Welcome to Arnold Schwarzenegger Movies!</h1>
-          </Route>
-          <Route path="/movies" exact component={MovieList} />
-          <Route path="/movies/:id" component={MovieDetail} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<h1>Welcome to Arnold Schwarzenegger Movies!</h1>} />
+          <Route path="/movies" element={<MovieList />} />
+          <Route path="/movies/:id" element={<MovieDetail />} />
+        </Routes>
       </div>
     </Router>
   );
