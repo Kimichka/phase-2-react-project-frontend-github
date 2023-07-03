7bc72b0fdb508dc615f3ce341cbfb16335f364a6
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-07-03 18:38:35 -0400

    Updates App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import MovieDetail from './components/MovieDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <h1>Welcome to Arnold Schwarzenegger Movies!</h1>
+          </Route>
+          <Route path="/movies" exact component={MovieList} />
+          <Route path="/movies/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
