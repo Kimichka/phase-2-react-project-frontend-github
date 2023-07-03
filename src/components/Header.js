3b126f18385bca8bacdf25c2da1c0e8719cf3d01
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-07-03 19:42:57 -0400

    Updates Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-function Header({ showMovies, setShowMovies }) {
+const Header = ({ showMovies, setShowMovies }) => {
+  const handleHomeClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    setShowMovies(false);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Arnold Movies</Link>
+        <span className="navbar-brand header-text">Arnold Schwarzenegger Movies</span>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link to="/" className="nav-link" onClick={() => setShowMovies(!showMovies)}>{showMovies ? 'Hide' : 'Movies'}</Link>
+              <button className="nav-link" onClick={handleHomeClick}>Home</button>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link" onClick={() => setShowMovies(true)}>Movies</button>
             </li>
             
           </ul>
