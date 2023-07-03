ea45129b072a1f655d07b871dacf2510f53e33eb
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-07-03 19:06:51 -0400

    Updates Files

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,9 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/movies">Movies</Link></li>
-        <li><Link to="/series">Series</Link></li>
-      </ul>
+      <Link to="/">Home</Link>
+      <Link to="/movies">Movies</Link>
+      
     </nav>
   );
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -10,13 +10,18 @@ const MovieList = () => {
       .then(data => setMovies(data));
   }, []);
 
+  if (movies.length === 0) return <div>Loading...</div>;
+
   return (
     <div>
+      <h1>Movie List</h1>
       {movies.map(movie => (
         <div key={movie.id}>
-          <h2><Link to={`/movies/${movie.id}`}>{movie.title}</Link></h2>
+          <h2>{movie.title}</h2>
           <p>Genre: {movie.genre}</p>
+          <p>Series: {movie.series}</p>
           <p>Rating: {movie.rating}</p>
+          <Link to={`/movies/${movie.id}`}>More Details</Link>
         </div>
       ))}
     </div>
