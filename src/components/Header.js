2b8736c3017fb3306a00844ae192b209b4933122
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-07-03 19:48:17 -0400

    Updates Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,19 +13,9 @@ const Header = ({ showMovies, setShowMovies }) => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <span className="navbar-brand header-text">Arnold Schwarzenegger Movies</span>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <button className="nav-link" onClick={handleHomeClick}>Home</button>
-            </li>
-            <li className="nav-item">
-              <button className="nav-link" onClick={() => setShowMovies(true)}>Movies</button>
-            </li>
-            
-          </ul>
+        <div className="navbar-buttons">
+          <button className="nav-link" onClick={handleHomeClick}>Home</button>
+          <button className="nav-link" onClick={() => setShowMovies(true)}>Movies</button>
         </div>
       </div>
     </nav>
