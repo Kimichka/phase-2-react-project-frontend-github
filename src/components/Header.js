a5b1a9b29303fb069fb08edfe421763ea606e1f7
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-07-03 20:36:34 -0400

    Updates header and title

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,19 +10,19 @@ const Header = ({ showMovies, setShowMovies }) => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      
-      <div className="container-fluid">
-      <div className="navbar-title">
-      
-          <span className="navbar-brand header-text">Arnold Schwarzenegger Movies</span>
-        </div>
-        <div className="navbar-buttons">
-          <button className="nav-link" onClick={handleHomeClick}>Home</button>
-          <button className="nav-link" onClick={() => setShowMovies(true)}>Movies</button>
-        </div>
+    <header className="header" style={{ backgroundColor: "#333", padding: "20px", color: "#fff" }}>
+      <div className="header-title" style={{ fontSize: "24px", fontWeight: "bold" }}>
+        Arnold Schwarzenegger Movies
       </div>
-    </nav>
+      <nav className="header-nav" style={{ display: "flex" }}>
+        <a href="#" className="header-link" style={{ marginLeft: "10px", color: "#fff", textDecoration: "none" }} onClick={handleHomeClick}>
+          Home
+        </a>
+        <a href="#" className="header-link" style={{ marginLeft: "10px", color: "#fff", textDecoration: "none" }} onClick={() => setShowMovies(true)}>
+          Movies
+        </a>
+      </nav>
+    </header>
   );
 }
 
