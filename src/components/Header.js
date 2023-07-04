6862e743c234959d571d26d693ae6422854a0463
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-07-03 20:41:04 -0400

    Updates Front End

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,12 +15,12 @@ const Header = ({ showMovies, setShowMovies }) => {
         Arnold Schwarzenegger Movies
       </div>
       <nav className="header-nav" style={{ display: "flex" }}>
-        <a href="#" className="header-link" style={{ marginLeft: "10px", color: "#fff", textDecoration: "none" }} onClick={handleHomeClick}>
+        <button className="header-link" style={{ marginLeft: "10px", color: "#fff", textDecoration: "none", background: "none", border: "none" }} onClick={handleHomeClick}>
           Home
-        </a>
-        <a href="#" className="header-link" style={{ marginLeft: "10px", color: "#fff", textDecoration: "none" }} onClick={() => setShowMovies(true)}>
+        </button>
+        <button className="header-link" style={{ marginLeft: "10px", color: "#fff", textDecoration: "none", background: "none", border: "none" }} onClick={() => setShowMovies(true)}>
           Movies
-        </a>
+        </button>
       </nav>
     </header>
   );
