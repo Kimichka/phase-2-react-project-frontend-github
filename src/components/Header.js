80642c67792a43b5ea027320a52ecdf8b6ed1bc6
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-07-03 20:06:20 -0400

    Updates header and css for looks

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,12 @@ const Header = ({ showMovies, setShowMovies }) => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      
       <div className="container-fluid">
-        <div className="navbar-brand header-text">Arnold Schwarzenegger Movies</div>
+      <div className="navbar-title">
+      
+          <span className="navbar-brand header-text">Arnold Schwarzenegger Movies</span>
+        </div>
         <div className="navbar-buttons">
           <button className="nav-link" onClick={handleHomeClick}>Home</button>
           <button className="nav-link" onClick={() => setShowMovies(true)}>Movies</button>
